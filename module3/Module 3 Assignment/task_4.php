3fbd298f1670ba303b5801e4682d8e6c7bbe7afe
ferdous11 <fzamancse11@gmail.com>
2023-09-26 20:45:45 +0600

    after fb post

--- module3/Module 3 Assignment/task_4.php
+++ module3/Module 3 Assignment/task_4.php
@@ -1,13 +1,26 @@
 <?php 
  $studentGrades= [
-                    "Student1"=>["Math"=>5.0,"English"=>4.5,"Science"=>4.5],
+                    "Student1"=>["Math"=>5.0,"English"=>5,"Science"=>5],
                     "Student2"=>["Math"=>4.5,"English"=>5.0,"Science"=>4.0],
                     "Student3"=>["Math"=>3.5,"English"=>4,"Science"=>3.5]
                  ];
 fnAverageGrade($studentGrades);
 function fnAverageGrade($studentGrades){
     foreach($studentGrades  as $key=>$arr){
-        $result[$key] = number_format(array_sum($arr)/3,2);
+        $result = number_format(array_sum($arr)/3,2);
+        if($result==5)
+            echo $key." A+\n";
+        else if($result>=4.5)
+            echo $key." A\n";
+        else if($result>=4)
+            echo $key." A-\n";
+        else if($result>=3.5)
+            echo $key." B\n";
+        else if($result>=3)
+            echo $key." C\n";
+        else if($result>=2.5)
+            echo $key." D\n";
+        else
+            echo $key." F\n";
     }
-    print_r($result);
 }  
--- module3/Module 3 Assignment/task_5.php
+++ module3/Module 3 Assignment/task_5.php
@@ -1,11 +1,20 @@
 <?php
-generatePassword(15);
+generatePassword(12);
 function generatePassword($length){
     $password = "";
-    $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
-    $count = mb_strlen($chars)-1;
-    for($i=0;$i<$length;$i++){
-        $password .= $chars[rand(0, $count)];
+    $smallLetter = "abcdefghijklmnopqrstuvwxyz";//26
+    $capitalLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";//26
+    $numerical = "0123456789";//10
+    $specalCharacter = "!@#$%^&*()_+";//12
+    $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";//74
+    
+    $password .= $smallLetter[rand(0, 25)];
+    $password .= $capitalLetter[rand(0, 25)];
+    $password .= $numerical[rand(0, 9)];
+    $password .= $specalCharacter[rand(0, 11)];
+    
+    for($i=4;$i<$length;$i++){
+        $password .= $chars[rand(0, 73)];
     }
     echo $password;
 }
